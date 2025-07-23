bc094de231ac84ce8142a48a64d86470ee0bb1aa
Ridho <sayidridho65@gmail.com>
2025-07-23 17:28:51 +0800

    [add & update] Process signup to database

---
+++ database.php
@@ -0,0 +1,17 @@
+<?php
+
+$host = "localhost";
+$dbname = "login_db";
+$username = "root";
+$password = "";
+
+$connection = new mysqli(hostname: $host,
+                     username: $username,
+                     password: $password,
+                     database: $dbname);
+                     
+if ($connection->connect_error) {
+    die("Connection error: " . $mysqli->connect_error);
+}
+
+return $connection;
--- process-signup.php
+++ process-signup.php
@@ -1,33 +1,57 @@
 <?php
 
-// write the process to register into database
-if(empty($_POST["name"])) {
-    die("Please fill the name!");
+if (empty($_POST["name"])) {
+    die("Name is required");
 }
 
 if ( ! filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)) {
     die("Valid email is required");
 }
 
-if(strlen($_POST["password"]) < 8) {
-    die("Password must be at least 8 characters!");
+if (strlen($_POST["password"]) < 8) {
+    die("Password must be at least 8 characters");
 }
 
 if ( ! preg_match("/[a-z]/i", $_POST["password"])) {
-    die("Password must contain at least one letter!");
+    die("Password must contain at least one letter");
 }
 
-if ( ! preg_match("/[0-9]/i", $_POST["password"])) {
-    die("Password must contain at least one number!");
+if ( ! preg_match("/[0-9]/", $_POST["password"])) {
+    die("Password must contain at least one number");
 }
 
-if($_POST["password"] !== $_POST["password_confirmation"]) {
-    die("Password does not match!");
+if ($_POST["password"] !== $_POST["password_confirmation"]) {
+    die("Passwords must match");
 }
 
 $password_hash = password_hash($_POST["password"], PASSWORD_DEFAULT);
 
-print_r($_POST);
-var_dump($password_hash);
+$connection = require __DIR__ . "/database.php";
 
-?>
+$sql = "INSERT INTO user (name, email, password_hash)
+        VALUES (?, ?, ?)";
+        
+$stmt = $connection->stmt_init();
+
+if ( ! $stmt->prepare($sql)) {
+    die("SQL error: " . $connection->error);
+}
+
+$stmt->bind_param("sss",
+                  $_POST["name"],
+                  $_POST["email"],
+                  $password_hash);
+                  
+if ($stmt->execute()) {
+
+    header("Location: signup-success.html");
+    exit;
+    
+} else {
+    
+    if ($connection->error === 1062) {
+        die("email already taken");
+    } else {
+        die($connection->error . " " . $connection->connect_error);
+    }
+}
